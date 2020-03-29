9bda014830c4abe1729dc6ed449ce5987b82fe81
cybersatan <golovinandy@gmail.com>
2020-03-29 21:54:27 +0300

    Java Syntax Level 4, Lection 4 Задача: Правило треугольника

--- 1.JavaSyntax/src/com/javarush/task/task04/task0415/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0415/Solution.java
@@ -3,13 +3,12 @@ package com.javarush.task.task04.task0415;
 /* 
 Правило треугольника
 */
-
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
+        //test
         Scanner scan = new Scanner(new InputStreamReader(System.in));
                 int rechA = scan.nextInt();
      int rechB = scan.nextInt();
@@ -22,6 +21,7 @@ public class Solution {
         else
 
             System.out.println("Треугольник не существует.");
+        //testing Git
 
         //напишите тут ваш код
 
