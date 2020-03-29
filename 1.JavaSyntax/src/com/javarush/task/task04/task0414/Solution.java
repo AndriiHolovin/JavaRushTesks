cc365d7b7f38aa95419ee2d2377be263ced5453e
cybersatan <golovinandy@gmail.com>
2020-03-29 21:43:31 +0300

    Java Syntax                                                                                     Level 4,                                                                                     Lection 4                                                                                     Задача: Правило треугольника

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0414/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task04.task0414;
+
+/* 
+Количество дней в году
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+
+    }
+}
--- 1.JavaSyntax/src/com/javarush/task/task04/task0415/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0415/Solution.java
@@ -4,8 +4,25 @@ package com.javarush.task.task04.task0415;
 Правило треугольника
 */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(new InputStreamReader(System.in));
+                int rechA = scan.nextInt();
+     int rechB = scan.nextInt();
+     int rechC =scan.nextInt();
+     scan.close();
+        if(rechA<rechB+rechC && rechB<rechA+rechC && rechC<rechA+rechB)
+            System.out.println("Треугольник существует.");
+
+
+        else
+
+            System.out.println("Треугольник не существует.");
+
         //напишите тут ваш код
 
     }
